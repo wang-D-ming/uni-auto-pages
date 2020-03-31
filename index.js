b62f5b3669dd63edcce8294b67910bd6c72fd97f
wangming15050380882 <727694556@qq.com>
2020-03-31 16:30:37 +0800

    优化

--- index.js
+++ index.js
@@ -2,33 +2,34 @@
  * @Author: wangming
  * @Date: 2020-03-26 21:56:30
  * @LastEditors: wangming
- * @LastEditTime: 2020-03-27 14:47:27
+ * @LastEditTime: 2020-03-31 16:29:19
  * @Description: file content
  */
-const chalk = require("chalk");
-const autoPages = require("./lib/auto-pages");
-const fs = require("fs-extra");
-const pluginName = "UniAutoPagesPlugin";
-const path = require("path");
+const chalk = require('chalk');
+const autoPages = require('./lib/auto-pages');
+const fs = require('fs-extra');
+const pluginName = 'UniAutoPagesPlugin';
+const path = require('path');
 class UniAutoPagesPlugin {
   constructor(temPath) {
     this.temPath = temPath;
   }
+  generate() {
+    const code = autoPages(this.temPath);
+    const to = path.resolve('src/pages.json');
+    if (
+      fs.existsSync(to) &&
+      fs.readFileSync(to, 'utf8').trim() === code.trim()
+    ) {
+      return;
+    }
+    fs.writeFileSync(to, code);
+    console.log(chalk.green('pages.json生成成功'));
+  }
   apply(compiler) {
-    const generate = () => {
-      const code = autoPages(this.temPath);
-      const to = path.resolve("src/pages.json");
-      if (
-        fs.existsSync(to) &&
-        fs.readFileSync(to, "utf8").trim() === code.trim()
-      ) {
-        return;
-      }
-      fs.writeFileSync(to, code);
-      console.log(chalk.green("pages.json生成成功"));
-    };
-    compiler.hooks.run.tap(pluginName, generate);
-    compiler.hooks.watchRun.tap(pluginName, generate);
+    compiler.hooks.run.tap(pluginName, this.generate.bind(this));
+    compiler.hooks.watchRun.tap(pluginName, this.generate.bind(this));
   }
 }
 module.exports = UniAutoPagesPlugin;
+// new UniAutoPagesPlugin().generate();
